ffedc0d3ab6d2e9a76b8a7311e9b4d2cd0349f91
CognerixStudios <cognerix@gmail.com>
2026-08-26 12:57:31 +0530

    feat: set up Capacitor for Android in apps/web client

---
+++ apps/web/android/app/src/main/java/com/cognerix/puzzlebattle/MainActivity.java
@@ -0,0 +1,5 @@
+package com.cognerix.puzzlebattle;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ apps/web/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cognerix.puzzlebattle',
+  appName: 'Cognerix',
+  webDir: 'dist'
+};
+
+export default config;
