49b461aeaa986e79678ff6586db2ef3d6c915eee
CognerixStudios <cognerix@gmail.com>
2026-08-30 13:14:16 +0530

    feat: configure Capacitor GoogleAuth plugin configurations in capacitor.config.ts

--- apps/web/capacitor.config.ts
+++ apps/web/capacitor.config.ts
@@ -3,7 +3,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.cognerix.puzzlebattle',
   appName: 'Cognerix',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    GoogleAuth: {
+      scopes: ['profile', 'email'],
+      serverClientId: '205247808441-cj93adqm6cb7kbcobi6bblg5tuq45tdj.apps.googleusercontent.com',
+      forceCodeForRefreshToken: true,
+    }
+  }
 };
 
 export default config;
